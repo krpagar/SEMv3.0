22e5f937654f0fc2ba02e21d0ee535f0094d88fa
krpagar <krpagar@gmail.com>
2021-04-22 12:20:42 +0530

    Update BMP280_I2C_WLC.ino

--- BMP280_I2C_WLC/BMP280_I2C_WLC.ino
+++ BMP280_I2C_WLC/BMP280_I2C_WLC.ino
@@ -1,30 +1,9 @@
-// BMx280_I2C.ino
-//
-// shows how to use the BMP280 / BMx280 library with the sensor connected using I2C.
-//
-// Copyright (c) 2018 Gregor Christandl
-//
-// connect the AS3935 to the Arduino like this:
-//
-// Arduino - BMP280 / BME280
-// 3.3V ---- VCC
-// GND ----- GND
-// SDA ----- SDA
-// SCL ----- SCL
-// some BMP280/BME280 modules break out the CSB and SDO pins as well: 
-// 5V ------ CSB (enables the I2C interface)
-// GND ----- SDO (I2C Address 0x76)
-// 5V ------ SDO (I2C Address 0x77)
-// other pins can be left unconnected.
 
 #include <Arduino.h>
 #include <Wire.h>
-
 #include <BMx280I2C.h>
 
 #define I2C_ADDRESS 0x76
-
-//create a BMx280I2C object using the I2C interface with I2C Address 0x76
 BMx280I2C bmx280(I2C_ADDRESS);
 const int GND = 17;
 
